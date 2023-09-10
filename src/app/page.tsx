046fcd240a5251f796c3b839b25b836ac150c907
Michael J R <michael.rubenstein@mail.utoronto.ca>
2023-09-10 17:12:27 -0400

    added inputs

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,14 +5,17 @@ import styles from './page.module.css'
 export default function Home() {
   return (
     <main className={styles.main}>
-      {/* <div className="form-group">
-        <input type='text'/>
-        <input type='phone'/>
-      </div> */}
-    <div className="form-group">
-      <label htmlFor="exampleFormControlInput1">姓名</label>
-      <input type="text" className="form-control" id="exampleFormControlInput1" placeholder="姓名" />
-     </div>
+
+      <div className="form-group">
+        <label htmlFor="exampleFormControlInput1">姓名</label>
+        <input type="text" className="form-control" id="exampleFormControlInput1" placeholder="姓名" />
+      </div>
+
+      <div>
+        <label htmlFor="exampleFormControlInput1">手机号码</label>
+        <input  className="form-control" id="exampleFormControlInput1" placeholder="手机号码" type="number" pattern="[0-9]*" inputMode="numeric" />
+      </div>
+
     </main>
   )
 }
