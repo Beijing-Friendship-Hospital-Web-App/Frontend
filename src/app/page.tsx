e3a5f8877da88c6f146c8c7edefc543adb521e32
Michael J R <michael.rubenstein@mail.utoronto.ca>
2023-09-10 17:13:01 -0400

    reformmated

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,17 +5,14 @@ import styles from './page.module.css'
 export default function Home() {
   return (
     <main className={styles.main}>
-
       <div className="form-group">
         <label htmlFor="exampleFormControlInput1">姓名</label>
         <input type="text" className="form-control" id="exampleFormControlInput1" placeholder="姓名" />
       </div>
-
       <div>
         <label htmlFor="exampleFormControlInput1">手机号码</label>
         <input  className="form-control" id="exampleFormControlInput1" placeholder="手机号码" type="number" pattern="[0-9]*" inputMode="numeric" />
       </div>
-
     </main>
   )
 }
